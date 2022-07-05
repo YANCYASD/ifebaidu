fc799266fef91624f26d88c8c2e2adb5b571a93c
Yancy <1036322011@qq.com>
2022-07-06 01:18:38 +0800

    no message

--- ifePractice/day-09-SimpleScrollSide/index.js
+++ ifePractice/day-09-SimpleScrollSide/index.js
@@ -1,32 +1,44 @@
 window.onload = function () {
-    var divs = document.querySelectorAll('main div')
-    var nav = document.querySelector('.nav')
-    var navs = []
-    for (var i = 0; i < divs.length; i++) {
-        var navItem = document.createElement('div')
-        navItem.classList.add('sub')
-        navItem.innerHTML = divs[i].className
-        navItem.dataset.scrollTop = divs[i].offsetTop
-        nav.appendChild(navItem)
-        navs.push(navItem)
+  var divs = document.querySelectorAll("main div");
+  var nav = document.querySelector(".nav");
+  var navs = [];
+  for (var i = 0; i < divs.length; i++) {
+    var navItem = document.createElement("div");
+    navItem.classList.add("sub");
+    navItem.innerHTML = divs[i].className;
+    navItem.dataset.scrollTop = divs[i].offsetTop;
+    divs[i].dataset.scrollTop = divs[i].offsetTop + divs[i].offsetHeight;
+    nav.appendChild(navItem);
+    navs.push(navItem);
+  }
+  for (let i = 0; i < navs.length; i++) {
+    navs[i].addEventListener("click", function () {
+      document.documentElement.scroll({
+        top: navs[i].dataset.scrollTop,
+        left: 0,
+        behavior: "smooth",
+      });
+    });
+  }
+  function clean(navs) {
+    for (var i = 0; i < navs.length; i++) {
+      navs[i].classList.remove("current");
     }
-    for (let i = 0; i < navs.length; i++) {
-        navs[i].addEventListener('click', function () {
-            document.documentElement.scroll({top:navs[i].dataset.scrollTop,left:0,behavior:"smooth"});
-        })
+  }
+  navs[0].classList.add("current");
+  // console.log(divs[1].offsetTop);
+  window.addEventListener("scroll", function () {
+    console.log(document.documentElement.scrollTop);
+    var scrollT = document.documentElement.scrollTop;
+    if (scrollT >= 0 && scrollT < 500) {
+      clean(navs);
+      navs[0].classList.add("current");
+    } else if (scrollT >= 500 && scrollT < 1800) {
+      clean(navs);
+      navs[1].classList.add("current");
+    } else if (scrollT >= 1800 && scrollT < 3000) {
+      clean(navs);
+      navs[2].classList.add("current");
     }
-    function clean(navs) {
-        for (var i = 0; i < navs.length; i++) {
-            navs[i].classList.remove('current')
-        }
-    }
-    navs[0].classList.add('current')
-    console.log(divs[1].offsetTop);
-    window.addEventListener('scroll', function () {
-        console.log(document.documentElement.scrollTop);
-        var scrollT = document.documentElement.scrollTop;
-        if (scrollT >= 0 && scrollT < 500) { clean(navs); navs[0].classList.add('current') }
-        else if (scrollT >= 500 && scrollT < 1500) { clean(navs); navs[1].classList.add('current') }
-        else if (scrollT >= 1500 && scrollT < 3000) { clean(navs); navs[2].classList.add('current') }
-    })
-}
+  });
+};
